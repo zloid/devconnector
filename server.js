2bf66284916de086a9c428b0afde3bc233118830
zloid <zloid-1@yandex.by>
2019-08-15 18:18:54 +0200

    mongodb test connect

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
-app.get('/', (req, res) => res.send('API Running idlz '))
+
+connectDB()
+
+app.get('/', (req, res) => res.send('API Running idlzz '))
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
