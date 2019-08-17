d7f6a9f6462f8a6a6dee0b25d0a342a4d91a4401
zloid <zloid-1@yandex.by>
2019-08-17 10:08:23 +0200

    routes tests created

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const app = express()
 connectDB()
 
 app.get('/', (req, res) => res.send('API Running idlzz '))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
