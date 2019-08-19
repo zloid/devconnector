e0c64f44a6524dea786ce73197e15164f0a5eae3
zloid <zloid-1@yandex.by>
2019-08-19 14:01:19 +0200

    mongodb-atlas creating users collection

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ mongoose.connect(db)
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useCreateIndex: true
     });
     console.log("MongoDB Connected....");
   } catch (err) {
